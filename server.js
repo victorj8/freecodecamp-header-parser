b7801e4898d96ce27d7ab2fa4da137242305f5da
Victor Camacho <victor.camacho@block.one>
2021-04-07 14:32:32 -0400

    request header microservice

--- server.js
+++ server.js
@@ -25,7 +25,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function (req, res) {
+  res.json({ipaddress: req.connection.remoteAddress,
+    "language": req.headers["accept-language"],
+    "software" : req.headers['user-agent']});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
